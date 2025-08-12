2d9afb4aca3b2ca43dbaa4873123d540efdf141e
James Esguerra <jamesesguerra025@gmail.com>
2025-08-12 15:14:09 +0800

    add allow any cors policy

--- JamesEsg.ReverseProxy/Program.cs
+++ JamesEsg.ReverseProxy/Program.cs
@@ -5,7 +5,20 @@ builder.WebHost.UseUrls("http://0.0.0.0:8080");
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAny", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAny");
 
 app.MapReverseProxy();
 
